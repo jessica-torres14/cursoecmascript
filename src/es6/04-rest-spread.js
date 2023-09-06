735e1daa6de84214ac5ad81650cc132ff47fb2b6
Jessica-29 <tjessica0729@gmail.com>
2023-09-06 10:27:35 -0500

    4

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -13,3 +13,32 @@ console.log(username,age);
 console.log(username, user.age);
 
 /* Spread operator */
+
+let person ={ name: 'Oscar', age:28};
+let country = 'Colombia';
+
+let data = { id:1, ...person, country};
+console.log(data);
+
+/* rest */
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+}
+sum(6,3,2,3)
+
+
+
+let json1 = {name: "Mr. Michi", food: "Pescado"}
+let json2={age: 12,color: "Blanco"}
+function solution(json1, json2){
+    return {...json1, ...json2}
+    
+}
+export function solution(json1 = { name: 'Mr. Michi', food: 'Pescado' }, json2 = { age: 12, color: 'Blanco' }) {
+  
+    return { ...json1, ...json2 };
+  }
+
+
